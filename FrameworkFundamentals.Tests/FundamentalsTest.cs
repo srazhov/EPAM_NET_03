bbf6450e9492d69a1fd23858df3822439c31d273
srazhov <65289085+srazhov@users.noreply.github.com>
2020-10-17 15:15:34 +0600

    Tasks 2-6 are done

--- FrameworkFundamentals.Tests/FundamentalsTest.cs
+++ FrameworkFundamentals.Tests/FundamentalsTest.cs
@@ -1,18 +1,69 @@
-using NUnit.Framework;
+//-----------------------------------------------------------------------
+// <copyright file="FundamentalsTest.cs" company="EPAM">
+// Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author>Srazhov Miras</author>
+//-----------------------------------------------------------------------
 
 namespace FrameworkFundamentals.Tests
 {
+    using System.Collections;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test of the <see cref="Fundamentals"/> class
+    /// </summary>
+    [TestFixture]
     public class FundamentalsTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        /// <summary>Test of the <see cref="Fundamentals.MakeProperTitle(string, string)"/> method</summary>
+        /// <param name="title">Title to change</param>
+        /// <param name="exceptions">Exceptions that method will ignore</param>
+        /// <returns>The proper title</returns>
+        [TestCase("a clash of KINGS", "a an the of", ExpectedResult = "A Clash of Kings")]
+        [TestCase("THE WIND IN THE WILLOWS", "The In", ExpectedResult = "The Wind in the Willows")]
+        [TestCase("the quick brown fox", ExpectedResult = "The Quick Brown Fox")]
+        public string MakeProperTitle(string title, string exceptions = "") => Fundamentals.MakeProperTitle(title, exceptions);
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        /// <summary>Test of the <see cref="Fundamentals.AddOrChangeUrlParameter(string, string)"/> method</summary>
+        /// <param name="url">URL to change</param>
+        /// <param name="keyValueParameter">Key that changes the main URL</param>
+        /// <returns>Modified URL</returns>
+        [TestCase("www.example.com", "key=value", ExpectedResult = "www.example.com?key=value")]
+        [TestCase("www.example.com?key=value", "key2=value2", ExpectedResult = "www.example.com?key=value&key2=value2")]
+        [TestCase("www.example.com?key=oldValue", "key=newValue", ExpectedResult = "www.example.com?key=newValue")]
+        [TestCase("www.example.com?", "key=newValue", ExpectedResult = "www.example.com?key=newValue")]
+        public string AddOrChangeUrlParameter(string url, string keyValueParameter) => Fundamentals.AddOrChangeUrlParameter(url, keyValueParameter);
+
+        /// <summary>Test of the <see cref="Fundamentals.UniqueInOrder(IEnumerable)"/> method</summary>
+        /// <param name="enumerable">Enumerable object</param>
+        /// <returns>Sequence with unique order</returns>
+        [TestCase("AAAABBBCCDAABBB", ExpectedResult = "ABCDAB")]
+        [TestCase("ABBCcAD", ExpectedResult = "ABCcAD")]
+        [TestCase("12233", ExpectedResult = "123")]
+        [TestCase("", ExpectedResult = "")]
+        [TestCase(new double[] { 1.1, 2.2, 2.2, 3.3 }, ExpectedResult = new double[] { 1.1, 2.2, 3.3 })]
+        public IEnumerable UniqueInOrder_RemovesSame(IEnumerable enumerable) => Fundamentals.UniqueInOrder(enumerable);
+
+        /// <summary>
+        /// Test of the <see cref="Fundamentals.ReverseWords(string)"/> method
+        /// </summary>
+        /// <param name="text">Text to reverse</param>
+        /// <returns>Reversed text</returns>
+        [TestCase("", ExpectedResult = "")]
+        [TestCase("The greatest victory is that which requires no battle", ExpectedResult = "battle no requires which that is victory greatest The")]
+        public string ReverseWords_Test_ReturnStr_With_Reversed_Words_Order(string text) => Fundamentals.ReverseWords(text);
+
+        /// <summary>
+        /// Test of the <see cref="Fundamentals.GetSumOfTwoNumbers(string, string)"/> method
+        /// </summary>
+        /// <param name="first">First argument</param>
+        /// <param name="second">Second argument</param>
+        /// <returns>Merged longs in string type</returns>
+        [TestCase("11096", "678904", ExpectedResult = "690000")]
+        [TestCase("38814", "-5678", ExpectedResult = "33136")]
+        [TestCase("Abs", "-71892", ExpectedResult = null)]
+        [TestCase("12345", "-71892.294", ExpectedResult = null)]
+        public string GetSumOfTwoNumbers_ReturnSumOfNumbers_InString(string first, string second) => Fundamentals.GetSumOfTwoNumbers(first, second);
     }
 }
--- FrameworkFundamentals/Fundamentals.cs
+++ FrameworkFundamentals/Fundamentals.cs
@@ -1,12 +1,138 @@
-﻿namespace NET_03
+﻿//-----------------------------------------------------------------------
+// <copyright file="Fundamentals.cs" company="EPAM">
+// Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author>Srazhov Miras</author>
+//-----------------------------------------------------------------------
+
+namespace FrameworkFundamentals
 {
-    using System;
-    
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
     /// <summary>
-    /// 
+    /// Class that holds realizations of tasks from Framework Fundamentals' 
     /// </summary>
     public static class Fundamentals
     {
+        /// <summary>
+        /// Corrects the title considering the exception
+        /// </summary>
+        /// <param name="title">Correcting title</param>
+        /// <param name="exceptions">Exception words</param>
+        /// <returns>Corrected title</returns>
+        public static string MakeProperTitle(string title, string exceptions = "")
+        {
+            string[] titleArr = title.Split(' ');
+            string[] excArr = exceptions.Split(' ');
 
+            for (int i = 0; i < titleArr.Length; i++)
+            {
+                string mask = titleArr[i][0] + titleArr[i].Substring(1).ToLower();
+                if (!excArr.Any(c => c == mask) || i == 0)
+                {
+                    titleArr[i] = char.ToUpper(titleArr[i][0]) + titleArr[i].Substring(1).ToLower();
+                }
+                else
+                {
+                    titleArr[i] = titleArr[i].ToLower();
+                }
+            }
+
+            return string.Join(' ', titleArr);
+        }
+
+        /// <summary>
+        /// Adds or changes the parameter of given URL
+        /// </summary>
+        /// <param name="url">URL to change</param>
+        /// <param name="keyValueParameter">Key to add or change</param>
+        /// <returns>Modified URL with extra keys</returns>
+        /// <exception cref="System.ArgumentException">URL is not supported</exception>
+        public static string AddOrChangeUrlParameter(string url, string keyValueParameter)
+        {
+            Regex urlRegex = new Regex(@"(http | http(s) ?://)?([\w-]+\.)+[\w-]+[.(\w)]+(\[\?%&=]*)?");
+            Regex keyRegex = new Regex(@"\w=\w");
+
+            if (!urlRegex.IsMatch(url) || !keyRegex.IsMatch(keyValueParameter))
+            {
+                throw new System.ArgumentException();
+            }
+
+            List<string> urlSplit = new List<string>(url.Split('?', '&').Where(c => !string.IsNullOrWhiteSpace(c)));
+            string[] keysSplit = keyValueParameter.Split('=');
+
+            if (urlSplit.Count == 1)
+            {
+                return urlSplit[0] + '?' + keyValueParameter;
+            }
+
+            int index = urlSplit.FindIndex((c) => c.Contains(keysSplit[0]));
+            if (index != -1)
+            {
+                urlSplit[index] = keyValueParameter;
+            }
+            else
+            {
+                urlSplit.Add(keyValueParameter);
+            }
+
+            string result = string.Join('&', urlSplit).Remove(0, urlSplit[0].Length + 1);
+            return urlSplit[0] + '?' + result;
+        }
+
+        /// <summary>Removes the same sequences in a row</summary>
+        /// <param name="enumerable">Enumerable object</param>
+        /// <returns>Unique sequence</returns>
+        public static IEnumerable UniqueInOrder(IEnumerable enumerable)
+        {
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            List<object> results = new List<object>();
+            object repeat = null;
+
+            while (enumerator.MoveNext())
+            {
+                if (!enumerator.Current.Equals(repeat))
+                {
+                    repeat = enumerator.Current;
+                    results.Add(enumerator.Current);
+                    yield return enumerator.Current;
+                }
+            }
+        }
+
+        /// <summary>Reverses given text with ' ' separator</summary>
+        /// <param name="text">This text</param>
+        /// <returns>Reversed text</returns>
+        public static string ReverseWords(string text)
+        {
+            StringBuilder result = new StringBuilder(text.Length);
+
+            var edit = text.Split(' ');
+            for (int i = edit.Length - 1; i >= 1; i--)
+            {
+                result.Append(edit[i] + " ");
+            }
+
+            result.Append(edit[0]);
+            return result.ToString();
+        }
+
+        /// <summary>Parses given strings to long type and calculates the result</summary>
+        /// <param name="first">First argument</param>
+        /// <param name="second">Second argument</param>
+        /// <returns>Merged long in string type</returns>
+        public static string GetSumOfTwoNumbers(string first, string second)
+        {
+            if (long.TryParse(first, out long resultFirst) && long.TryParse(second, out long resultSecond))
+            {
+                return (resultFirst + resultSecond).ToString();
+            }
+
+            return null;
+        }
     }
 }
